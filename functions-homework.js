b650e8c9d8dedf3c75af840711ba445da43c8027
Kuba Klin <poczta@kubaklin.pl>
2024-10-15 10:50:19 +0200

    feat: homework tasks

---
+++ functions-homework.js
@@ -0,0 +1,44 @@
+// 1
+function getGreetings(name) {
+  alert('Hi, ' + name + ' What`s up?');
+}
+//getGreetings('kuba klin');
+
+// 2
+function isThisMyName(personName) {
+  if (personName == true) return true;
+  return false;
+}
+console.log(isThisMyName(''));
+
+// 3
+function isThisBoolean(testValue) {
+  if (typeof testValue == 'boolean') return true;
+  return false;
+}
+console.log(isThisBoolean(false))
+
+// 4
+function getCircleArea(radius) {
+  return 2 * Math.PI * radius;
+}
+console.log(getCircleArea(10));
+
+// 5
+function getCirclesAreaSum(radiusOne, radiusTwo) {
+  return getCircleArea(radiusOne) +  getCircleArea(radiusTwo);
+}
+console.log(getCirclesAreaSum(5, 10))
+
+// 6
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1;
+  } if (month <= 6) {
+    return 2;
+  } if (month <= 9) {
+    return 3;
+  }
+  return 4;
+}
+console.log (quarterOf(4));
